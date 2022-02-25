bd8ce1382e482dc906e65b5f3604d5b0e7df8283
Dan Stoakes <dan.stoakes8@gmail.com>
2022-02-25 23:42:17 +0000

    Initial commit

---
+++ finder.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# Written on the 25th February 2022 by Dan Stoakes
+
+DIR=$( cd "$(dirname "$0")" ; pwd -P )
+
+info()
+{
+	echo "Usage: \`./$DIR [flags]\`"
+	echo "\nwhere flags include:"
+	echo "\t-h <display help message>"
+	echo "\t-s <target file size to check against>"
+	echo "\t-d <specify a directory to traverse>"
+	echo "\t-f <file path for results to be written to>"
+}
+
+while getopts 'hs:d:f:' flag; do
+	case "$flag" in
+		h) info
+		   exit
+		   ;;
+		s) size=${OPTARG}
+		   ;;
+		d) directory=${OPTARG}
+		   ;;
+		f) file=${OPTARG}
+		   ;;
+	esac
+done
+
+if [ -z "$size" ]; then
+	size=1G
+fi
+
+if [ -z "$directory" ]; then
+	directory=/
+fi
+
+cd "$directory"
+echo "Finding files in directory $directory"
+
+if [ "$file" ]; then
+	echo "Writing to file $file"
+	du -ht "$size" "$directory" 2>/dev/null | sort -nr > "$file"
+else
+	du -ht "$size" "$directory" 2>/dev/null | sort -nr
+fi
